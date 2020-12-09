b7f829d0ad545d9729289ae6cfd0be70cb5277df
kevino29 <varax297@gmail.com>
2020-12-09 13:56:24 -0400

    fixed the image not loading problem

--- app/src/main/java/com/example/assignment3/ItemDetailFragment.java
+++ app/src/main/java/com/example/assignment3/ItemDetailFragment.java
@@ -1,15 +1,21 @@
 package com.example.assignment3;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.assignment3.dummy.DummyContent;
@@ -60,11 +66,15 @@ public class ItemDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.item_detail, container, false);
+//        View rootView = inflater.inflate(R.layout.item_detail, container, false);
+        View rootView = inflater.inflate(R.layout.item_image, container, false);
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.imagePath);
+//            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.imagePath);
+            int imageResource = this.getContext().getResources().getIdentifier(mItem.imagePath, "drawable", this.getContext().getPackageName());
+            System.out.println(imageResource);
+            ((ImageView) rootView.findViewById(R.id.item_image)).setImageResource(imageResource);
         }
 
         return rootView;
